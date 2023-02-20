41a7298d4942503a20ec7146d7ad0180a8cf9abe
Mark Huota <mark@markhuot.com>
2023-02-20 17:30:50 +0000

    fixing type

--- admin/actions/component/create.ts
+++ admin/actions/component/create.ts
@@ -1,6 +1,6 @@
 import {db} from "remixCms/utils/db";
 
-export async function createComponent(type)
+export async function createComponent({ type }: { type: string })
 {
     return db.component.create({ data: { type }});
 }
--- app/routes/admin/components.create.$handle.tsx
+++ app/routes/admin/components.create.$handle.tsx
@@ -1,9 +1,11 @@
-import {redirect} from "@remix-run/node";
+import {ActionArgs, redirect} from "@remix-run/node";
 import {createComponent} from "remixCms/actions/component/create";
 
-export async function loader()
+export async function loader({ params: { handle } }: ActionArgs)
 {
-    return redirect('/admin/components/edit/' + (await createComponent()).id);
+    return redirect('/admin/components/edit/' + (await createComponent({
+        type: handle!
+    })).id);
 }
 export default function Edit()
 {
